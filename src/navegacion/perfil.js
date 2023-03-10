32e8594a454765cfceda2f9546b210536c47208b
Meyli01 <isamayimeyli@gmail.com>
2023-03-09 21:01:01 -0600

    mey se equivoco jijiji

--- src/navegacion/perfil.js
+++ src/navegacion/perfil.js
@@ -2,19 +2,20 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import "./css/perfil.css";
 import { MdBloodtype } from "react-icons/md";
+import { SlBadge } from "react-icons/sl";
 const Perfil = () => {
   return (
     <div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-3">
-            </div>
             <Sidebar />
-            <div className="col-9">
-              <div className="card">
-                <MdBloodtype className="w-100 h-100" />
-              </div>
+          </div>
+          <div className="col-6">
+            <div className="card">
+              <MdBloodtype className="w-100 h-100" />
             </div>
+          </div>
           </div>
         </div>
     </div>
