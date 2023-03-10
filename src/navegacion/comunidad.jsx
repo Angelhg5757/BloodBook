2c6b4b26d74a4f2d35cf7cb1ddd24c29a59757ce
Angel Hernandez Gomez <angelhg1405@gmail.com>
2023-03-09 21:20:49 -0600

    Formato a Comunidad (Publicaciones)

--- src/navegacion/comunidad.jsx
+++ src/navegacion/comunidad.jsx
@@ -2,6 +2,13 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 const Comunidad = () => {
     const [data, setApiData] = useState([]);
 
@@ -22,22 +29,39 @@ const Comunidad = () => {
 
     return (
         <>
+         <Sidebar />
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <Sidebar />
+                    <div className="col-5">
                     </div>
-                    <div className="col">
-                        <div className="">
+                        <div className="col-6" >
                             {data.map((item, index) => (
-                                    <div className="col-6" key={index}>
-                                        <h2>{item.titulo}</h2>
-                                        <p>{item.descripcion}</p>
-                                        <input type="submit" value="Contactar" className="buttons2" />
-                                    </div>
+                            <Card sx={{ maxWidth: 345, backgroundColor: "#212121", color: "#fafafa", marginBottom: "15px" }} key={index}>
+                                <CardMedia
+                                    component="img"
+                                    // alt="green iguana"
+                                    height="140"
+                                    image="https://img.freepik.com/vector-premium/mujer-acostada-cama-hospital-ilustracion-personajes-dibujos-animados-personas-sobre-fondo-blanco-joven-que-padece-enfermedad-imagen-sala-electrocardiografo-tema-medico-sanitario_89615-1063.jpg?w=2000"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {item.titulo}
+                                    </Typography>
+                                    <Typography sx={{color: "#fafafa"}} variant="h7" color="text.secondary">
+                                        {item.nombre}{' '}{item.apePat}{' '}{item.apeMat}
+                                    </Typography>
+                                    <Typography sx={{color: "#fafafa"}} variant="body1" color="text.secondary">
+                                        {item.descripcion}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button variant="contained" size="medium" type="submit">Contactar</Button>
+                                    {/* <Button size="small">Learn More</Button> */}
+                                </CardActions>
+                            </Card>
                             ))}
                         </div>
-                    </div>
+                    
                 </div>
             </div>
         </>
